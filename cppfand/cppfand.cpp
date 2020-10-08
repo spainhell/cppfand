079f195a788d8febcb20052a46d07de350a54374
Jan Špaňhel <spainhell@gmail.com>
2020-10-08 08:45:50 +0200

    odstraneni logovani

--- cppfand/cppfand.cpp
+++ cppfand/cppfand.cpp
@@ -2,18 +2,11 @@
 #include "base.h"
 #include "drivers.h"
 #include "runfand.h"
-#include "../Logging/spdlog.h"
 #include <memory>
 
-using namespace ::std;
-namespace spd = spdlog;
 
 int main(int argc, char* argv[])
 {
-	std::shared_ptr<spd::logger> logger;
-	
-	logger->debug("STARTING APPLICATION");
-	
 	for (int i = 0; i < argc; i++)
 	{
 		paramstr.push_back(argv[i]);
