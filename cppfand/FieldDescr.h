ad5f6224366b1ed484edf8441962a40c0f1d9721
Jan Spanhel <spanhel@spanhel.eu>
2023-04-07 21:49:43 +0200

    prepare FieldDescr for refactoring

--- cppfand/FieldDescr.h
+++ cppfand/FieldDescr.h
@@ -26,14 +26,21 @@ public:
 	FieldDescr(BYTE* inputStr);
 	FieldDescr(const FieldDescr& orig);
 	FieldType field_type = FieldType::UNKNOWN;
-	bool field_flag = false;
-	char frml_type = 0;
-	BYTE L = 0, M = 0, NBytes = 0, Flg = 0;
-	// case boolean {Stored} of True:(Displ:short); False:(Frml:FrmlPtr; Name:string[1]{ curr.length });
-	short Displ = 0;
-	FrmlElem* Frml = nullptr;
 	std::string Name;
 	std::string Mask; // added! mask for item 'A' or 'D'
+	bool field_flag = false;
+	char frml_type = 0;
+	BYTE L = 0;
+	BYTE M = 0;
+	BYTE NBytes = 0;
+	BYTE Flg = 0;
+	short Displ = 0;
+	FrmlElem* Frml = nullptr;
+
+	/* std::string S; // A, N, T
+	double R;      // F, D, R
+	bool B;        // B */
+
 	static FieldType GetFieldType(char type);
 	static char GetFieldTypeChar(FieldType type);
 };
