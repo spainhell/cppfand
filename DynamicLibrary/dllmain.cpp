2069d0bcd7f0931dccaee3c0567d60149999ed7a
Jan Spanhel <spanhel@spanhel.eu>
2023-06-13 22:16:05 +0200

    new project for dll generate added

---
+++ DynamicLibrary/dllmain.cpp
@@ -0,0 +1,27 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <stdio.h>
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+extern "C"
+{
+	__declspec(dllexport) void Hello()
+	{
+        printf("Hello from CppFand DLL !\n");
+	}
+}
